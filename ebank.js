3f1c79c64fd138620cf293a08f3945d9e2e92c0e
Farhadcode <farhad.ahmed.aust@gmail.com>
2021-08-16 08:54:18 +0600

    add submit btn

---
+++ ebank.js
@@ -0,0 +1,18 @@
+document.getElementById('submitBtn').addEventListener('click',SubmitData= ()=>{
+    //email
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+    // console.log(userEmail);
+
+    //password
+       
+    const passwordField = document.getElementById('user-password');
+    const userPassword =passwordField.value;
+    // console.log(userPassword);
+
+    if ( userEmail == 'ebank@gmail.com' &&  userPassword =='password') {
+        console.log('valide user');
+        window.location.href = 'banking.html';
+        
+    }
+})
